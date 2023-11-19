9931263b56f06f92030918332738c2fda701ca93
KazumaTakata <k.takata@qufooit.com>
2023-11-19 21:02:31 +0900

    fix bug

--- cmd/root.go
+++ cmd/root.go
@@ -14,11 +14,9 @@ var (
 	userLicense string
 
 	rootCmd = &cobra.Command{
-		Use:   "cobra-cli",
-		Short: "A generator for Cobra based Applications",
-		Long: `Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+		Use:   "static-site-go ",
+		Short: "small static site generator",
+		Long:  `small static site generator`,
 	}
 )
 
